3f3e0765b83128371025db876edb9e62b6d21636
ProgrammerAvinash <62152948+ProgrammerAvinash@users.noreply.github.com>
2020-04-20 04:05:51 +0530

    Updates

---
+++ precache-manifest.86d882c429f5e1a08ce71e91e87d1970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9f73d15f9c34903f4797189eddc034",
+    "url": "/My_Portfolio/index.html"
+  },
+  {
+    "revision": "1d0e945153bedd62405f",
+    "url": "/My_Portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "22a648749b0f6c49444f",
+    "url": "/My_Portfolio/static/css/main.04d16707.chunk.css"
+  },
+  {
+    "revision": "1d0e945153bedd62405f",
+    "url": "/My_Portfolio/static/js/2.fabe1491.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/My_Portfolio/static/js/2.fabe1491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a648749b0f6c49444f",
+    "url": "/My_Portfolio/static/js/main.4b8990b0.chunk.js"
+  },
+  {
+    "revision": "258c0c782379ac63f083",
+    "url": "/My_Portfolio/static/js/runtime-main.5fb9201a.js"
+  }
+]);
